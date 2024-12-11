60efca1cc79c1b9daeac3407dca9d6d6d41a1bc5
Phillip-Gao <phillipgao10@gmail.com>
2024-12-11 12:32:32 -0500

    Update config file

--- server/db-config.js
+++
@@ -1,8 +0,0 @@
-// Private. Will not be included in submission
-module.exports = {
-  host: "database-1.cbwu8ge8e3tl.us-east-1.rds.amazonaws.com",
-  port: "3300",
-  user : "postgres",
-  password: "philgao123",
-  database: "Friends_and_Family_DB"
-};
